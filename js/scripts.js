49bdccab204046aa79dbce9a68f695ce0f427ca4
zohaibbukhari126 <zohaibbukhari126@gmail.com>
2024-03-17 07:10:15 +0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the navbar toggler element
+    const navbarToggler = document.querySelector('.navbar-toggler');
+
+    // Add a click event listener to it
+    navbarToggler.addEventListener('click', function() {
+        // Check if the navbar is already expanded
+        const isExpanded = navbarToggler.getAttribute('aria-expanded') === 'true';
+
+        // Set aria-expanded attribute to the opposite value
+        navbarToggler.setAttribute('aria-expanded', !isExpanded);
+    });
+
+    // Add click event listeners to navbar links
+    document.querySelectorAll('.navbar-nav .nav-link').forEach(item => {
+        item.addEventListener('click', () => {
+            // Close the navbar on mobile when a link is clicked
+            navbarToggler.click();
+        });
+    });
+});
